a9dc124b9f0d6da817d4c080ea0decfeeb055f83
Thibault Giraudon <tgiraudo@z2r10p6.42lyon.fr>
2022-11-16 16:48:34 +0100

    norme

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 19:05:56 by tgiraudo          #+#    #+#             */
-/*   Updated: 2022/11/16 16:24:32 by tgiraudo         ###   ########.fr       */
+/*   Updated: 2022/11/16 16:42:34 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,35 @@ int	ft_printf(const char *s, ...)
 		if (s[i] == '%' && s[i + 1])
 		{
 			i++;
-			if (s[i] == 'c')
-				n += ft_putchar(va_arg(args, int));
-			else if (s[i] == 's')
-				n += ft_putstr(va_arg(args, char *));
-			else if (s[i] == 'p')
-				n += print_adress(va_arg(args, void *));
-			else if (s[i] == 'd')
-				n += ft_putnbr(va_arg(args, int));
-			else if (s[i] == 'i')
-				n += ft_putnbr(va_arg(args, int));
-			else if (s[i] == 'u')
-				n += ft_putnbr_undec(va_arg(args, unsigned int));
-			else if (s[i] == 'x')
-				n += ft_putnbr_base(va_arg(args, int), "0123456789abcdef");
-			else if (s[i] == 'X')
-				n += ft_putnbr_base(va_arg(args, int), "0123456789ABCDEF");
-			else if (s[i] == '%')
-				n += ft_putchar('%');
+			n += ft_print_type(args, s[i]);
 		}
 		else
 			n += ft_putchar(s[i]);
 		i++;
 	}
+	va_end(args);
 	return (n);
+}
+
+int	ft_print_type(va_list args, char c)
+{
+	if (c == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	if (c == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	if (c == 'p')
+		return (print_adress(va_arg(args, void *)));
+	if (c == 'd')
+		return (ft_putnbr(va_arg(args, int)));
+	if (c == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	if (c == 'u')
+		return (ft_putnbr_undec(va_arg(args, unsigned int)));
+	if (c == 'x')
+		return (ft_putnbr_base(va_arg(args, int), "0123456789abcdef"));
+	if (c == 'X')
+		return (ft_putnbr_base(va_arg(args, int), "0123456789ABCDEF"));
+	if (c == '%')
+		return (ft_putchar('%'));
+	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,12 +6,12 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 19:06:23 by tgiraudo          #+#    #+#             */
-/*   Updated: 2022/11/16 16:11:43 by tgiraudo         ###   ########.fr       */
+/*   Updated: 2022/11/16 16:45:57 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include<stdarg.h>
 # include<unistd.h>
@@ -25,5 +25,6 @@ int		ft_putnbr_base(long int nb, char *tab);
 int		print_adress(void *addr);
 int		ft_putnbr_undec(unsigned int n);
 int		ft_strlen(char *s);
+int		ft_print_type(va_list args, char c);
 
 #endif
